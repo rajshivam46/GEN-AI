617010d758b8ca4a4d032dfaa8d9c285c31a0084
rajshivam46 <142870215+rajshivam46@users.noreply.github.com>
2026-05-01 00:12:13 +0530

    Create experiment-3.py

---
+++ experiment-3.py
@@ -0,0 +1,26 @@
+import tensorflow as tf
+from tensorflow.keras import layers, models
+
+# Load dataset
+(x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
+
+x_train = x_train / 255.0
+x_test = x_test / 255.0
+
+# Model
+model = models.Sequential([
+    layers.Flatten(input_shape=(28,28)),
+    layers.Dense(128, activation='relu'),
+    layers.Dense(10, activation='softmax')
+])
+
+# Compile
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+# Train
+model.fit(x_train, y_train, epochs=5)
+
+# Evaluate
+model.evaluate(x_test, y_test)
